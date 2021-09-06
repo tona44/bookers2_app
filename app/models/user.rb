2634226291c21d98008d0f94e8f9964f200b7b2f
tona44 <kuryu.waku@gmail.com>
2021-09-06 11:26:43 +0000

    [fix] 応用課題4：フォローフォロワー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   
   
-  has_many :relationships, foreign_key: :following_id
+  has_many :relationships, foreign_key: :following_id, dependent: :destroy
   has_many :followings, through: :relationships, source: :follower
   
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :following
 
   def is_followed_by?(user)
