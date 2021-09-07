44a1b9587351dd98d5dbbad94c627836c5ac9e52
tona44 <kuryu.waku@gmail.com>
2021-09-07 03:12:37 +0000

    [add] 課題5：検索機能

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,41 @@
+class SearchesController < ApplicationController
+  
+  def search
+    @model = params["search"]["model"]
+    @method = params["search"]["method"]
+    @content = params["search"]["content"]
+    @records = search_for(@model, @method, @content)
+  end
+  
+  
+private
+  
+  def search_for(model, method, content)
+    
+    if model == 'user'
+      if method == 'perfect_match'    #完全一致
+        User.where(name: content)
+      elsif method == 'forward_match'     #前方一致
+        User.where("name LIKE ?", content+'%' )
+      elsif method == 'backward_match'    #後方一致
+        User.where("name LIKE ?", '%'+content )
+      elsif method == 'partial_match'     #部分一致
+        User.where("name LIKE ?", '%'+content+'%' )
+      end
+    
+    elsif model == 'book'
+      if method == 'perfect_match'    #完全一致
+        Book.where(title: content)
+      elsif method == 'forward_match'     #前方一致
+        Book.where("title LIKE ?", content+'%' )
+      elsif method == 'backward_match'    #後方一致
+        Book.where("title LIKE ?", '%'+content )
+      elsif method == 'partial_match'     #部分一致
+        Book.where("title LIKE ?", '%'+content+'%' )
+      end
+    end
+    
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'favorites/create'
-  get 'favorites/destroy'
   devise_for :users
+
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
 
   resources :users, only:[:index,:show,:edit,:update,:create] do
     resource :relationships, only:[:create,:destroy]
-    get :followings, on: :member
-    get :followers, on: :member
+    get :followings, on: :member  # users直下の[:id(PK)]が付与される
+    get :followers, on: :member   # users直下の[:id(PK)]が付与される
   end
 
   resources :books do  #ネストが好ましい
@@ -18,5 +17,8 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create,:destroy]
        #複数あるコメントの内、どのコメントを削除するかのコメント[/:id]が必要なので、複数形
   end
+
+  get '/search' => 'searches#search'
+
 
 end
