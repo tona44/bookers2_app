fcfa3aa1a8c5c293dc5740a4478cad057cc22846
tona44 <kuryu.waku@gmail.com>
2021-09-08 07:03:07 +0000

    [fix] 応用課題6：いいね非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,24 +3,19 @@ class FavoritesController < ApplicationController
   
   def create
     @book = Book.find(params[:book_id])
-    favorite = @book.favorites.new(user_id: current_user.id)
-      #上記2行は favorite = current_user.favorites.new(book_id: book.id) に省略可
+    favorite = @book.favorites.new(user_id: current_user.id)  #上記2行は favorite = current_user.favorites.new(book_id: book.id) に省略可
     favorite.save
-    
-    # redirect_to request.referer
-      #非同期通信の為削除する（備忘の為コメントアウト）
-      #render先は自動的に favorites/create.js.erb となる（コントローラー名/アクション名.js.erb）
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    favorite = @book.favorites.find_by(user_id: current_user.id)
-      #上記2行は favorite = current_user.favorites.find_by(book_id: book.id)　に省略可
+    favorite = @book.favorites.find_by(user_id: current_user.id)  #上記2行は favorite = current_user.favorites.find_by(book_id: book.id)　に省略可
     favorite.destroy
-    
-    # redirect_to request.referer
-      #非同期通信の為削除する（備忘の為コメントアウト）
-      #render先は自動的に favorites/create.js.erb となる（コントローラー名/アクション名.js.erb）
   end
+  
+    # redirect_to request.referer は非同期通信の為削除する
+    # render先は自動的に favorites/create(destroy).js.erb となる（コントローラー名/アクション名.js.erb）
+
+  
   
 end
